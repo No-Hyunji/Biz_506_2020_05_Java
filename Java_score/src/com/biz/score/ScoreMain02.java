385156242cf11394eca56cf9548c9731a64a609b
No-Hyunji <65649079+No-Hyunji@users.noreply.github.com>
2020-07-02 16:59:00 +0900

    Add files via upload

---
+++ Java_score/src/com/biz/score/ScoreMain02.java
@@ -0,0 +1,21 @@
+package com.biz.score;
+
+import com.biz.score.service.ScoreService02;
+
+public class ScoreMain02 {
+	public static void main(String[] args) {
+		
+		ScoreService02 scoreService02 = new ScoreService02();
+		
+		while(true) {
+			if(!scoreService02.inputScore())
+				break;
+		
+
+		
+	}
+		System.out.println("입력완료");
+		scoreService02.scoreList();
+	}
+
+}
