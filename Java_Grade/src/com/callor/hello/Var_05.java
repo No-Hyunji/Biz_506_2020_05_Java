c34fc5a9708fe89a39d2b4225a2519c291669bf1
No-Hyunji <kiekuy47@gmail.com>
2020-09-29 13:47:53 +0900

    JAVA

--- Java_Grade/src/com/callor/hello/Var_05.java
+++ Java_Grade/src/com/callor/hello/Var_05.java
@@ -5,11 +5,16 @@ import java.util.Random;
 public class Var_05 {
 	
 	public static void main(String[] args) {
-		int num1, num2;
-		Random rnd = new Random();
-		num1 = rnd.nextInt(100)+1;
-		num2 = rnd.nextInt(100)+1; 
 		
+		Random rnd = new Random();
+		
+		int num1 = rnd.nextInt(100)+1;
+		int num2 = rnd.nextInt(100)+1; 
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
 		
 	}
 
--- Java_Grade/src/com/callor/hello/Var_06.java
+++ Java_Grade/src/com/callor/hello/Var_06.java
@@ -1,6 +1,20 @@
 package com.callor.hello;
 
+import java.util.Random;
+
 public class Var_06 {
 	
+	public static void main(String[] args) {
+		Random rnd = new Random();
+		int num = rnd.nextInt(100)+1;
+		
+		int intSum = 0;
+		for(int i = 1 ; i < 100 ; i ++ ) {
+			if(num % 3 == 0) {
+				intSum += num;
+			}
+			System.out.println("3의 배수의 합" + intSum);
+		}
+	}
 
 }
