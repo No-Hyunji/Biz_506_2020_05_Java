73b788abcb2c5689f2b5252e1719b2de40edea9c
No-Hyunji <kiekuy47@gmail.com>
2020-11-02 16:37:20 +0900

    JAVA

---
+++ Java_Test/src/com/biz/test/GuguEx.java
@@ -0,0 +1,12 @@
+package com.biz.test;
+
+public class GuguEx {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 0 ; i < 10; i ++) {
+			
+		}
+	}
+
+}
---
+++ Java_Test/src/com/biz/test/ListEx.java
@@ -0,0 +1,9 @@
+package com.biz.test;
+
+public class ListEx {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
